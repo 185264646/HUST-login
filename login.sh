48f58aadf412edb6ec195ef42ea9e8adfdb5ecc9
185264646 <43387518+185264646@users.noreply.github.com>
2023-10-27 16:09:31 +0800

    login.sh: enable set -eu
    make script more robust

--- login.sh
+++ login.sh
@@ -339,12 +339,14 @@ parse_args() {
 	return 0
 }
 
+set -eu
+
 trap exit_handler exit
 
 parse_args "$@" || { print_syntax "$0"; exit 2; }
 
-redir_url="$(get_cur_network_state)"
-case $? in
+{ redir_url="$(get_cur_network_state)"; ret=$?; }
+case $ret in
 	0)
 		log "Already connected to Internet"
 		exit 0
